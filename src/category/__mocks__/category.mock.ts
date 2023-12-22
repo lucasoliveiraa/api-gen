f63217fcb17d3dece45750b436300074d41d4670
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-12-21 23:09:22 -0300

    ✔️ feat: create test unit category

---
+++ src/category/__mocks__/category.mock.ts
@@ -0,0 +1,8 @@
+import { CategoryEntity } from '../entities/category.entity';
+
+export const categoryMock: CategoryEntity = {
+  id: 'awdawd-awdwad-awdawd12331-adw',
+  name: 'createCategoryMock',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
